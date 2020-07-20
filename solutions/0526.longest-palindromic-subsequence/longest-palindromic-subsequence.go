05de6e0a0d3e8a3d82830f1d3e329046cf9c2fb3
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-20 22:18:18 +0800

    add 0516.longest-palindromic-subsequence

---
+++ solutions/0526.longest-palindromic-subsequence/longest-palindromic-subsequence.go
@@ -0,0 +1,27 @@
+package problem0516
+
+func LongestPalindromeSubseq(s string) int {
+	max := func(x, y int) int {
+		if x > y {
+			return x
+		}
+		return y
+	}
+	n := len(s)
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+		dp[i][i] = 1
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][n-1]
+}
