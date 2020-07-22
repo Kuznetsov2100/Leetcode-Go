e378be7a57396fde796f1b679f203c8cb68b4ffa
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-22 21:46:13 +0800

    add solution for problem309

---
+++ solutions/309.最佳买卖股票时机含冷冻期.go
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=309 lang=golang
+ *
+ * [309] 最佳买卖股票时机含冷冻期
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	n := len(prices)
+	if n <= 1 {
+		return 0
+	}
+	// dp[i][0]: in day i, not hold stock
+	// dp[i][1]: in day i, hold stock
+	// dp[n-1][0]: in last day, we got the maximum profit after stock has been sold.
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, 2)
+	}
+	// base case
+	// dp[-1][0] = 0
+	// dp[-1][1] = -infinity
+	for i := 0; i < n; i++ {
+		if i == 0 {
+			dp[i][0] = 0
+			// dp[i][0] = max(dp[-1][0], dp[-1][1]+prices[0])
+			// dp[i][0] = max(0, -infinity + prices[0]) = 0
+			dp[i][1] = -prices[0]
+			// dp[i][1] = max(dp[-1][1],dp[-1][0]-prices[0])
+			// dp[i][1] = max(-infinity, 0-prices[0]) = -prices[0]
+			continue
+		}
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		// dp[i][0] = max(choose rest, choose sell)
+		if i == 1 {
+			dp[i][1] = max(dp[0][1], dp[0][0]-prices[i]) 
+			continue
+		}
+		// 第 i 天(i > 1)选择 buy 的时候，要从 i-2 的状态转移，而不是 i-1 。
+		dp[i][1] = max(dp[i-1][1], dp[i-2][0]-prices[i]) 
+		// dp[i][1] = max(choose rest, choose buy)
+	}
+	return dp[n-1][0]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+// @lc code=end
+
