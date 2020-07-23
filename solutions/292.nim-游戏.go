2ec008f23355cc941e06b2b4439adca540023fca
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-23 21:39:12 +0800

    add solution for problem292

---
+++ solutions/292.nim-游戏.go
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=292 lang=golang
+ *
+ * [292] Nim 游戏
+ */
+
+// @lc code=start
+func canWinNim(n int) bool {
+	if n % 4 == 0 {
+		return false
+	} 
+	return true
+}
+// @lc code=end
+
