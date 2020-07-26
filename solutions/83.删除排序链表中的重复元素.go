6f881f9d5fd3a80c07367f2b4c0860e78c1b48fc
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-26 19:08:31 +0800

    add solution for problem83

---
+++ solutions/83.删除排序链表中的重复元素.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=golang
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	current := head
+	for current != nil {
+		// 删除所有重复元素后再移动到下一个元素
+		for current.Next != nil && current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+		}
+		current = current.Next
+	}
+	return head
+}
+// @lc code=end
+
