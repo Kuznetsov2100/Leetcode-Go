d984d41fc0f25adc979d3f7bf6a2dc24429c8fdf
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-26 11:56:55 +0800

    update comments

--- solutions/141.环形链表.go
+++ solutions/141.环形链表.go
@@ -16,7 +16,7 @@ func hasCycle(head *ListNode) bool {
 	// 解释：快指针每次前进两步，慢指针每次前进一步。如果不含有环，快指针最终会遇到nil，说明链表不含环；
 	// 如果含有环，快指针最终会超慢指针一圈，和慢指针相遇，说明链表含有环。
 	fast, slow := head,head
-	for fast != nil && fast.Next != nil {
+	for fast != nil  && fast.Next != nil { // fast.Next != nil 是为了确保下方的fast.Next.Next是合法的指针
 		fast = fast.Next.Next
 		slow = slow.Next
 		if fast == slow {
