e29104c7e3ca28a1518b45747408bf6a6465dc53
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-28 16:35:40 +0800

    add solution for problem98

---
+++ solutions/98.验证二叉搜索树.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=golang
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+	return helper(root, nil,nil)
+}
+
+func helper(root,min,max *TreeNode) bool {
+	// root需要对整棵左子树和右子树进行约束
+	if root == nil {
+		return true
+	}
+	if min != nil && root.Val <= min.Val {
+		return false
+	}
+	if max != nil && root.Val >= max.Val {
+		return false
+	}
+	return helper(root.Left,min,root) && helper(root.Right,root,max)
+}
+// @lc code=end
+
