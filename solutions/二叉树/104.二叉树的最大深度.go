5ceda963ad61861c0fba3a01fb33d61a3b629f8d
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-28 20:37:02 +0800

    add solution for problem104

---
+++ solutions/二叉树/104.二叉树的最大深度.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=golang
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	return max(maxDepth(root.Left)+1,maxDepth(root.Right)+1)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+// @lc code=end
+
