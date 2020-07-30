095218be2fac48e89a3381cb6f9dfc0e128b0d4d
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-30 22:09:14 +0800

    add solution for problem513

---
+++ solutions/二叉树/513.找树左下角的值.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=513 lang=golang
+ *
+ * [513] 找树左下角的值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findBottomLeftValue(root *TreeNode) int {
+	var queue []*TreeNode
+	var node *TreeNode	
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		node = queue[0]
+		queue = queue[1:]
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+		if node.Left != nil {
+			queue = append(queue,node.Left)
+		}
+	}
+	return node.Val
+	
+}
+// @lc code=end
+
