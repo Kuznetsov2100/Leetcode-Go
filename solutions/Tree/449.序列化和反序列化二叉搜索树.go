3f3fdd692f774dc921e5c66c4a57e59135b95f70
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-31 17:57:28 +0800

    add solution for problem449

---
+++ solutions/Tree/449.序列化和反序列化二叉搜索树.go
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=449 lang=golang
+ *
+ * [449] 序列化和反序列化二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ import "strings"
+ import "strconv"
+ type Codec struct {
+	 null string
+	 sep string
+	 nodes []string
+ }
+ 
+ func Constructor() Codec {
+	 return Codec{null:"#", sep:","} 
+ }
+ 
+ // Serializes a tree to a single string.
+ func (this *Codec) serialize(root *TreeNode) string {
+		 var sb strings.Builder
+		 this.serializeHelper(root, &sb)
+		 return sb.String()
+ }
+ 
+ func (this *Codec) serializeHelper(root *TreeNode, sb *strings.Builder) {
+	 if root == nil {
+		 sb.WriteString(this.null)
+		 sb.WriteString(this.sep)
+		 return 
+	 }
+	 sb.WriteString(strconv.Itoa(root.Val))
+	 sb.WriteString(this.sep)
+	 this.serializeHelper(root.Left, sb)
+	 this.serializeHelper(root.Right, sb)
+ }
+ 
+ // Deserializes your encoded data to tree.
+ func (this *Codec) deserialize(data string) *TreeNode {    
+	 this.nodes = strings.Split(data, this.sep)
+	 return this.deserializeHelper()
+ }
+ 
+ func (this *Codec) deserializeHelper() *TreeNode {
+	 if len(this.nodes) == 0 {
+		 return nil
+	 }
+	 first := this.nodes[0]
+	 this.nodes = this.nodes[1:]
+	 if first == this.null {
+		 return nil
+	 }
+	 intVal, err := strconv.Atoi(first)
+	 if err != nil {
+		 panic(err)
+	 }
+	 root := &TreeNode{Val:intVal}
+	 root.Left = this.deserializeHelper()
+	 root.Right = this.deserializeHelper()
+	 return root
+ }
+
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * obj := Constructor();
+ * data := obj.serialize(root);
+ * ans := obj.deserialize(data);
+ */
+// @lc code=end
+
