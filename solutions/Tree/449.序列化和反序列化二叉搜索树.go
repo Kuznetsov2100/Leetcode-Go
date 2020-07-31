94f6aa996624c9cf866adc74b4148fa61500fef0
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-31 19:21:17 +0800

    make the code stateless

--- solutions/Tree/449.序列化和反序列化二叉搜索树.go
+++ solutions/Tree/449.序列化和反序列化二叉搜索树.go
@@ -13,63 +13,56 @@
  *     Right *TreeNode
  * }
  */
- import "strings"
- import "strconv"
- type Codec struct {
-	 null string
-	 sep string
-	 nodes []string
- }
- 
- func Constructor() Codec {
-	 return Codec{null:"#", sep:","} 
- }
- 
- // Serializes a tree to a single string.
- func (this *Codec) serialize(root *TreeNode) string {
-		 var sb strings.Builder
-		 this.serializeHelper(root, &sb)
-		 return sb.String()
- }
- 
- func (this *Codec) serializeHelper(root *TreeNode, sb *strings.Builder) {
-	 if root == nil {
-		 sb.WriteString(this.null)
-		 sb.WriteString(this.sep)
-		 return 
-	 }
-	 sb.WriteString(strconv.Itoa(root.Val))
-	 sb.WriteString(this.sep)
-	 this.serializeHelper(root.Left, sb)
-	 this.serializeHelper(root.Right, sb)
- }
- 
- // Deserializes your encoded data to tree.
- func (this *Codec) deserialize(data string) *TreeNode {    
-	 this.nodes = strings.Split(data, this.sep)
-	 return this.deserializeHelper()
- }
- 
- func (this *Codec) deserializeHelper() *TreeNode {
-	 if len(this.nodes) == 0 {
-		 return nil
-	 }
-	 first := this.nodes[0]
-	 this.nodes = this.nodes[1:]
-	 if first == this.null {
-		 return nil
-	 }
-	 intVal, err := strconv.Atoi(first)
-	 if err != nil {
-		 panic(err)
-	 }
-	 root := &TreeNode{Val:intVal}
-	 root.Left = this.deserializeHelper()
-	 root.Right = this.deserializeHelper()
-	 return root
- }
+import "strings"
+import "strconv"
+type Codec struct {
+}
 
+func Constructor() Codec {
+	return Codec{} 
+}
 
+// Serializes a tree to a single string.
+func (this *Codec) serialize(root *TreeNode) string {
+	var sb strings.Builder
+	var serializeHelper func(root *TreeNode)
+	serializeHelper = func(root *TreeNode) {
+		if root == nil {
+			sb.WriteString("#")
+			sb.WriteString(",")
+			return 
+		}
+		sb.WriteString(strconv.Itoa(root.Val))
+		sb.WriteString(",")
+		serializeHelper(root.Left)
+		serializeHelper(root.Right)
+	}
+	serializeHelper(root)
+	return sb.String()
+}
+
+// Deserializes your encoded data to tree.
+func (this *Codec) deserialize(data string) *TreeNode {    
+	nodes := strings.Split(data, ",")
+	var deserializeHelper func() *TreeNode
+	deserializeHelper = func() *TreeNode {
+		if len(nodes) == 0 {
+			return nil
+		}
+		first := nodes[0]
+		nodes = nodes[1:]
+		if first == "#" {
+			return nil
+		}
+		intVal, _ := strconv.Atoi(first)
+		return &TreeNode{
+			Val: intVal,
+			Left: deserializeHelper(),
+			Right: deserializeHelper(),
+		}
+	}
+	return deserializeHelper()
+}
 /**
  * Your Codec object will be instantiated and called as such:
  * obj := Constructor();
