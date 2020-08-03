0b4b1c49a538c7bb140de35d24e06ea578fca921
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-03 12:48:54 +0800

    update

--- solutions/Tree/896.单调数列.go
+++ solutions/Tree/896.单调数列.go
@@ -5,60 +5,19 @@
  */
 
 // @lc code=start
-import "sort"
-type TreeNode struct {
-    Val int
-    Left *TreeNode
-    Right *TreeNode
-}
-
 func isMonotonic(A []int) bool {
-	B := make([]int, len(A))
-	copy(B,A)
-	for i, j := 0, len(A)-1; i < j; i, j = i+1, j-1 {
-		A[i], A[j] = A[j], A[i]
+	positive,negative := false,false
+	for i := 0; i < len(A)-1; i++ {
+		if A[i] < A[i+1] {
+			positive = true
+		} else if A[i] > A[i+1] {
+			negative = true
+		}
+		if positive && negative {
+			return false
+		}
 	}
-	if sort.IntsAreSorted(A) {
-		tree := sortedArrayToBST(A)
-		return isValidBST(tree)
-	} else {
-		tree := sortedArrayToBST(B)
-		return isValidBST(tree)
-	}
-}
-
-
-func sortedArrayToBST(nums []int) *TreeNode {
-	return buildTree(nums, 0, len(nums)-1)
-}
-
-func buildTree(nums []int, lo, hi int) *TreeNode {
-	if lo > hi {
-		return nil
-	}
-	mid := lo + (hi-lo)/2
-	root := &TreeNode{Val:nums[mid]} // 以数组中点作为根节点
-	root.Left = buildTree(nums,lo, mid-1) // 递归构建左子树
-	root.Right = buildTree(nums,mid+1, hi) // 递归构建右子树
-	return root
-}
-
-func isValidBST(root *TreeNode) bool {
-	return helper(root, nil,nil)
-}
-
-func helper(root,min,max *TreeNode) bool {
-	// root需要对整棵左子树和右子树进行约束
-	if root == nil {
-		return true
-	}
-	if min != nil && root.Val < min.Val {
-		return false
-	}
-	if max != nil && root.Val > max.Val {
-		return false
-	}
-	return helper(root.Left,min,root) && helper(root.Right,root,max)
+	return true
 }
 // @lc code=end
 
