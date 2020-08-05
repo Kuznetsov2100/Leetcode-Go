4d998b23929f259c237aadbfb8272990622ff365
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-05 19:16:13 +0800

    add solution for problem131

---
+++ solutions/Backtracking/131.分割回文串.go
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=131 lang=golang
+ *
+ * [131] 分割回文串
+ */
+
+// @lc code=start
+func partition(s string) [][]string {
+	var res [][]string
+	if s == "" {
+		return res
+	}
+	var backtrack func(str string, track []string)
+	backtrack = func(str string, track []string) {
+		if str == "" && len(track) > 0 {
+			tmp := make([]string, len(track))
+			copy(tmp,track)
+			res = append(res, tmp)
+			return
+		}
+		for i := 1; i <= len(s); i++ {
+			if i > len(str) {
+				return
+			}
+			if i > 1 && !isPalindrome(str[:i]) {
+				continue
+			}
+			track = append(track, str[:i])
+			
+			backtrack(str[i:],track)
+			track = track[:len(track)-1]
+		}
+	}
+	backtrack(s,[]string{})
+	return res
+}
+
+func isPalindrome(s string) bool {
+	i, j := 0, len(s)-1
+	for i < j {
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+// @lc code=end
+
