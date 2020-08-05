7a83debd1b49e71fdfb4099b81173cb8f2c6b6f8
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-05 20:46:41 +0800

    update solution for problem131

--- solutions/Backtracking/131.分割回文串.go
+++ solutions/Backtracking/131.分割回文串.go
@@ -7,11 +7,11 @@
 // @lc code=start
 func partition(s string) [][]string {
 	var res [][]string
-	if s == "" {
+	length := len(s)
+	if length == 0 {
 		return res
 	}
-	isPalindrome := func(s string) bool {
-		i, j := 0, len(s)-1
+	isPalindrome := func(i int, j int) bool {
 		for i < j {
 			if s[i] != s[j] {
 				return false
@@ -21,27 +21,24 @@ func partition(s string) [][]string {
 		}
 		return true
 	}
-	var backtrack func(str string, track []string)
-	backtrack = func(str string, track []string) {
-		if str == "" && len(track) > 0 {
+	var backtrack func(pos int, substrlen int, track []string)
+	backtrack = func(pos int, substrlen int, track []string) {
+		if substrlen == 0 {
 			tmp := make([]string, len(track))
 			copy(tmp,track)
 			res = append(res, tmp)
 			return
 		}
-		for i := 1; i <= len(s); i++ {
-			if i > len(str) {
-				return
-			}
-			if i > 1 && !isPalindrome(str[:i]) {
+		for i := 1; i <= substrlen; i++ {
+			if i > 1 && !isPalindrome(pos,pos+i-1) {
 				continue
 			}
-			track = append(track, str[:i])
-			backtrack(str[i:],track)
+			track = append(track, s[pos:pos+i])
+			backtrack(pos+i,substrlen-i,track)
 			track = track[:len(track)-1]
 		}
 	}
-	backtrack(s,[]string{})
+	backtrack(0,length,[]string{})
 	return res
 }
 // @lc code=end
