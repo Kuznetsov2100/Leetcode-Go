7ad98eeda24a5d3ef649ca9254f99631b7ad5ac8
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-06 12:27:13 +0800

    update solution for problem216

--- solutions/Backtracking/216.组合总和-iii.go
+++ solutions/Backtracking/216.组合总和-iii.go
@@ -10,29 +10,26 @@ func combinationSum3(k int, n int) [][]int {
 	if k <= 0 || n <= 0 {
 		return res
 	}
-	var backtrack func(start,sum, pre int, track []int)
-	backtrack = func(start,sum, pre int, track []int) {
+	var backtrack func(start,sum int, track []int)
+	backtrack = func(start,sum int, track []int) {
 		if sum == n && len(track) == k { 
 			tmp := make([]int, len(track))
 			copy(tmp, track)	
 			res = append(res, tmp)
 			return		
 		}
-		for i := start; i <= 9; i++ {
-			if i <= pre {
-				continue
-			}
+		for i := start; i <= 9; i++ {		
 			sum += i
 			if sum > n {
-				continue
+				return
 			}
 			track = append(track, i)
-			backtrack(start+1,sum,i,track)
+			backtrack(i+1,sum,track)
 			track = track[:len(track)-1]
 			sum -= i					
 		}
 	}
-	backtrack(1,0,-101, []int{})
+	backtrack(1,0,[]int{})
 	return res
 }
 // @lc code=end
