fd455d05a5595b76ac2d20313afb9b7068651985
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-08 18:08:22 +0800

    add solution for problem538

---
+++ solutions/Tree/538.把二叉搜索树转换为累加树.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=538 lang=golang
+ *
+ * [538] 把二叉搜索树转换为累加树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func convertBST(root *TreeNode) *TreeNode {
+	var sum int
+	var reverse func(root *TreeNode)
+	reverse = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		reverse(root.Right)
+		sum += root.Val
+		root.Val = sum
+		reverse(root.Left)
+	}
+	reverse(root)
+	return root
+}
+// @lc code=end
+
