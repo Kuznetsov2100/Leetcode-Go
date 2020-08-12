6de968aaf7bcc3b9a3af341a9bc20769b4cdad89
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-12 20:41:37 +0800

    add solution for problem520

---
+++ solutions/string/520.检测大写字母.go
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=520 lang=golang
+ *
+ * [520] 检测大写字母
+ */
+
+// @lc code=start
+import "strings"
+func detectCapitalUse(word string) bool {
+	if word == strings.ToLower(word) || word == strings.ToUpper(word) || word == word[0:1]+strings.ToLower(word[1:]) {
+		return true
+	}
+	return false
+}
+// @lc code=end
+
