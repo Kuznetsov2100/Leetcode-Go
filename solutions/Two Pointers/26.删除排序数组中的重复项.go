ca52c46de9e65ead2af808f3a2e70ea443e7b1c5
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-16 17:59:55 +0800

    add solution for problem26

---
+++ solutions/Two Pointers/26.删除排序数组中的重复项.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=golang
+ *
+ * [26] 删除排序数组中的重复项
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	slow, fast := 0, 1 // 快慢指针
+	// 我们让慢指针 slow 走左后面，快指针 fast 走在前面探路，找到一个不重复的元素就告诉 slow 并让 slow 前进一步。
+	// 这样当 fast 指针遍历完整个数组 nums 后，nums[0..slow] 就是不重复元素，之后的所有元素都是重复元素。
+	for fast < len(nums) {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+	return slow+1
+}
+// @lc code=end
+
