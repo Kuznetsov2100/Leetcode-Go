2ebe8b2135d1a1893ad1944cad4c9d88014f564d
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-17 19:20:09 +0800

    update solution for problem389

--- solutions/Hash Table/389.找不同.go
+++ solutions/Hash Table/389.找不同.go
@@ -8,14 +8,14 @@
 func findTheDifference(s string, t string) byte {
 	var res byte
 	smap := [26]int{}
-	for i := range s {
-		smap[s[i]-'a']++
-	}
 	for i := range t {
-		smap[t[i]-'a']--
+		smap[t[i]-'a']++
+		if i < len(s) {
+			smap[s[i]-'a']--
+		}	
 	}
 	for i := range smap {
-		if smap[i] < 0 {
+		if smap[i] > 0 {
 			res = byte(i+97)
 			break
 		}
