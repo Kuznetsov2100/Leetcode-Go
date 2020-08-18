c5d6d06df0bd2ea9d3f788492b203f506d134598
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-18 11:09:49 +0800

    add solution for problem27

---
+++ solutions/Array/27.移除元素.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=golang
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+func removeElement(nums []int, val int) int {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
+// @lc code=end
+
