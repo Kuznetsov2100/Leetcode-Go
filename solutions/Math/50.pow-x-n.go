07d3ea947427613c1d3d15e9438b92412f1b0bad
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-19 13:08:55 +0800

    update solution for problem50

--- solutions/Math/50.pow-x-n.go
+++ solutions/Math/50.pow-x-n.go
@@ -6,19 +6,40 @@
 
 // @lc code=start
 func myPow(x float64, n int) float64 {
+	// 快速幂算法
 	if n == 0 {
 		return 1.0
 	}
 	if n < 0 {
-		x = 1/x
 		n = -n
+		x = 1/x
 	}
-	res := myPow(x, n/2)
-	if n % 2 != 0 {
-		return res * res * x
-	} else {
-		return res * res
+	res := 1.0
+	for n > 0 {
+		if n & 1 == 1 { // 如果n为奇数,需要多乘以一个x
+			res *= x
+		}
+		x *= x
+		n >>= 1 // n/2 
 	}
+	return res
 }
+
+// 递归版本
+// func myPow(x float64, n int) float64 {
+// 	if n == 0 {
+// 		return 1.0
+// 	}
+// 	if n < 0 {
+// 		x = 1/x
+// 		n = -n
+// 	}
+// 	res := myPow(x, n >> 1)
+// 	if n & 1 == 1 {
+// 		return res * res * x
+// 	} else {
+// 		return res * res
+// 	}
+// }
 // @lc code=end
 
