34408889a3a88b22aa5363dc070ba0a883aea5da
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-20 18:54:38 +0800

    add solution for problem374

---
+++ solutions/Binary Search/374.猜数字大小.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=374 lang=golang
+ *
+ * [374] 猜数字大小
+ */
+
+// @lc code=start
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+func guessNumber(n int) int {
+	left, right := 1, n
+	for left <= right {
+		mid := left + (right - left)/2
+		if guess(mid) == -1 {
+			right = mid-1
+		} else if guess(mid) == 1 {
+			left = mid+1
+		} else if guess(mid) == 0 {
+			return mid
+		}
+	}
+	return -1
+}
+// @lc code=end
+
