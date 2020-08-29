2b07d40d3a98c774cea117c2839e3823e3d5cb9c
handane123 <7983700+handane123@users.noreply.github.com>
2020-08-29 12:46:04 +0800

    update solution for problem234

--- solutions/Linked List/234.回文链表.go
+++ solutions/Linked List/234.回文链表.go
@@ -13,18 +13,26 @@
  * }
  */
 func isPalindrome(head *ListNode) bool {
-	var arr []int
-	for head != nil {
-		arr = append(arr, head.Val)
-		head = head.Next
+	slow, fast := head, head // 快慢指针找中点
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
 	}
-	i, j := 0, len(arr)-1
-	for i < j {
-		if arr[i] != arr[j] {
+	// 反转以中点为头节点的后半段链表
+	var pre *ListNode // 反转以后的链表的头节点
+	for slow != nil {
+		next := slow.Next
+		slow.Next = pre
+		pre = slow
+		slow = next
+	}
+	// 依次比较head和pre值是否相同
+	for head != nil && pre != nil {
+		if head.Val != pre.Val {
 			return false
 		}
-		i++
-		j--
+		head = head.Next
+		pre = pre.Next
 	}
 	return true
 }
