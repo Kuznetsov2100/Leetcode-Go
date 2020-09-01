805eefbe615436724880f7433a8a01e75f808025
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-01 19:07:41 +0800

    update solution for problem347

--- solutions/Hash Table/347.前-k-个高频元素.go
+++ solutions/Hash Table/347.前-k-个高频元素.go
@@ -5,26 +5,41 @@
  */
 
 // @lc code=start
-import "sort"
+import "container/heap"
 func topKFrequent(nums []int, k int) []int {
-	type foo struct {
-		number int
-		frequency int
-	}
-	var arr []foo
 	var res []int
 	m := make(map[int]int)
 	for i := range nums {
 		m[nums[i]]++
 	}
-	for k, v := range m {
-		arr = append(arr, foo{number: k, frequency: v})
+	h := &MaxHeap{}
+	heap.Init(h)
+	for number, frequency := range m {
+		heap.Push(h, &element{value:number, frequency:frequency})
 	}
-	sort.Slice(arr, func(i, j int) bool { return arr[i].frequency > arr[j].frequency })
-	for i := 0; i < k; i++ {
-		res = append(res,arr[i].number)
+	for k > 0 && h.Len() > 0 {
+		res = append(res, heap.Pop(h).(*element).value)
+		k--
 	}
 	return res
+}
+
+type element struct {
+	value int
+	frequency int
+}
+
+type MaxHeap []*element
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i].frequency > h[j].frequency }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *MaxHeap) Push(x interface{}) { *h = append(*h, x.(*element)) }
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
 }
 // @lc code=end
 
