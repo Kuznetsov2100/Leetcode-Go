97bdd1061773eb36317e4f2d2cc842220edc5389
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-05 21:59:18 +0800

    add solution for problem57

---
+++ solutions/Sort/57.插入区间.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=57 lang=golang
+ *
+ * [57] 插入区间
+ */
+
+// @lc code=start
+func insert(intervals [][]int, newInterval []int) [][]int {
+	return merge(append(intervals, newInterval))
+}
+
+func merge(intervals [][]int) [][]int {
+	// 数组按照每个区间的左端点升序排序
+	sort.Slice(intervals, func(i, j int) bool { return intervals[i][0] < intervals[j][0] })
+	var res [][]int
+	for i := 0; i < len(intervals); {
+		rightbound := intervals[i][1] // 初始化区间的右端点
+		j := i+1
+		for j < len(intervals) && intervals[j][0] <= rightbound {
+			if intervals[j][1] > rightbound {  // 拓展区间的右端点
+				rightbound = intervals[j][1]
+			}
+			j++
+		}
+		res = append(res, []int{intervals[i][0], rightbound}) // 将一个合并后的区间加入到结果数组
+		i = j
+	}
+	return res
+}
+// @lc code=end
+
