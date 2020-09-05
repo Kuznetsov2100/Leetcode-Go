945ab23424201251e61270f6a5bfe8394abe2e80
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-05 13:59:07 +0800

    update solution for problem494

--- solutions/DFS/494.目标和.go
+++ solutions/DFS/494.目标和.go
@@ -8,19 +8,19 @@
 func findTargetSumWays(nums []int, S int) int {
 	// 深度优先搜索+备忘录
 	m := make(map[[2]int]int) // array in golang can be compared, so array can used as key in map
-	var dfs func(i, substraction int) int
-	dfs = func(i, substraction int) int {
+	var dfs func(i, remainder int) int
+	dfs = func(i, remainder int) int {
 		if i == len(nums) {
-			if substraction == 0 {
+			if remainder == 0 {
 				return 1
 			}
 			return 0
 		}
-		if val, ok := m[[2]int{i, substraction}]; ok {
+		if val, ok := m[[2]int{i, remainder}]; ok {
 			return val
 		}
-		ans := dfs(i+1, substraction+nums[i]) + dfs(i+1, substraction-nums[i])
-		m[[2]int{i, substraction}] = ans
+		ans := dfs(i+1, remainder+nums[i]) + dfs(i+1, remainder-nums[i])
+		m[[2]int{i, remainder}] = ans
 		return ans
 	}
 	return dfs(0, S)
