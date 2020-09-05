8c45f6350a349fdf2240413ab7f6488eac16622c
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-05 18:05:46 +0800

    update solution for problem27

--- solutions/Array/27.移除元素.go
+++ solutions/Array/27.移除元素.go
@@ -6,14 +6,25 @@
 
 // @lc code=start
 func removeElement(nums []int, val int) int {
-	slow := 0
-	for fast := 0; fast < len(nums); fast++ {
-		if nums[fast] != val {
-			nums[slow] = nums[fast]
-			slow++
+	// 定义nums[0,index)为没有元素等于val的数组区间
+	index := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[index] = nums[i]
+			index++
 		}
 	}
-	return slow
+	return index // index-1-0+1
 }
+// func removeElement(nums []int, val int) int {
+// 	slow := 0
+// 	for fast := 0; fast < len(nums); fast++ {
+// 		if nums[fast] != val {
+// 			nums[slow] = nums[fast]
+// 			slow++
+// 		}
+// 	}
+// 	return slow
+// }
 // @lc code=end
 
