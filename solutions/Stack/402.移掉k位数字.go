98f3299d8bcf42d783b195edd95a5afbbad109a9
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-11 17:47:00 +0800

    update solution for problem402

--- solutions/Stack/402.移掉k位数字.go
+++ solutions/Stack/402.移掉k位数字.go
@@ -6,35 +6,54 @@
 
 // @lc code=start
 func removeKdigits(num string, k int) string {
-	var stack []int // 单调递增栈,栈中存储的是num字符串中的字符索引
-	var res []byte
+	var stack []byte // 单调递增栈,栈中存储的是num字符串中的字符
+	var res string
 	for i := range num {
-		for k > 0 && len(stack) > 0 && num[stack[len(stack)-1]] > num[i] {
+		for k > 0 && len(stack) > 0 && stack[len(stack)-1] > num[i] {
 			stack = stack[:len(stack)-1]
 			k--
 		}
-		stack = append(stack, i)
+		stack = append(stack, num[i])
 	}
-
-	// 如果k没用完且栈非空，继续出栈
-	for k > 0 && len(stack) > 0 {
-		stack = stack[:len(stack)-1]
-		k--
-	}
-	for i := range stack {
-		res = append(res, num[stack[i]])
-	}
-
-	// 跳过结果前缀可能存在的前导零
-	j := 0
-	for j < len(res) && res[j] == '0' {
-		j++
-	}
-	ans := string(res[j:])
-	if ans == "" {
+	// 如果k没有用完，截取前面len(stack)-k个元素，这相当于从stack中pop了k个元素
+	// 然后使用TrimLeft去除前导零
+	res = strings.TrimLeft(string(stack[:len(stack)-k]), "0")
+	if res == "" {
 		return "0"
 	}
-	return ans
+	return res
 }
+
+// func removeKdigits(num string, k int) string {
+// 	var stack []int // 单调递增栈,栈中存储的是num字符串中的字符索引
+// 	var res []byte
+// 	for i := range num {
+// 		for k > 0 && len(stack) > 0 && num[stack[len(stack)-1]] > num[i] {
+// 			stack = stack[:len(stack)-1]
+// 			k--
+// 		}
+// 		stack = append(stack, i)
+// 	}
+
+// 	// 如果k没用完且栈非空，继续出栈
+// 	for k > 0 && len(stack) > 0 {
+// 		stack = stack[:len(stack)-1]
+// 		k--
+// 	}
+// 	for i := range stack {
+// 		res = append(res, num[stack[i]])
+// 	}
+
+// 	// 跳过结果前缀可能存在的前导零
+// 	j := 0
+// 	for j < len(res) && res[j] == '0' {
+// 		j++
+// 	}
+// 	ans := string(res[j:])
+// 	if ans == "" {
+// 		return "0"
+// 	}
+// 	return ans
+// }
 // @lc code=end
 
