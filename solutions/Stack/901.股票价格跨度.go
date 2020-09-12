0fefb6eb12415a0374259d6a035ebbf417f9b75a
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-12 17:31:24 +0800

    update solution for problem901

--- solutions/Stack/901.股票价格跨度.go
+++ solutions/Stack/901.股票价格跨度.go
@@ -5,10 +5,9 @@
  */
 
 // @lc code=start
+type pair [2]int
 type StockSpanner struct {
-	prices []int // 单调递减栈，栈中保存的是price
-	span []int // 常规栈， 栈中保存的是prices中每个股票的价格跨度
-	
+	prices []pair // 单调递减栈，栈内元素为pair， pair[0]为价格，pair[1]对应是价格跨度
 }
 
 
@@ -18,16 +17,16 @@ func Constructor() StockSpanner {
 
 
 func (this *StockSpanner) Next(price int) int {
-	res := 1
-	for len(this.prices) > 0 && this.prices[len(this.prices)-1] <= price {
+	res := 1 // 价格跨度包含当天，所以初始化为1
+	for len(this.prices) > 0 && this.prices[len(this.prices)-1][0] <= price {
+		res += this.prices[len(this.prices)-1][1] // pair[1]为跨度
 		this.prices = this.prices[:len(this.prices)-1]
-		res += this.span[len(this.span)-1]
-		this.span = this.span[:len(this.span)-1]
+		
 	}
-	this.prices = append(this.prices, price)
-	this.span = append(this.span, res)
+	this.prices = append(this.prices, pair{price, res})
 	return res
 }
+
 
 
 /**
