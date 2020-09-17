2f2388e75932927882cbd015cd6830756eeba73e
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-17 11:44:46 +0800

    update solution for problem560

--- solutions/Array/560.和为k的子数组.go
+++ solutions/Array/560.和为k的子数组.go
@@ -7,14 +7,14 @@
 // @lc code=start
 func subarraySum(nums []int, k int) int {
 	var res, sum int
-	m := make(map[int]int)
-	m[0] = 1
+	m := make(map[int]int) // key:前缀和的值 value:出现次数
+	m[0] = 1 // 开始遍历前， 记录前缀和为0的次数为1
 	for i := range nums {
-		sum += nums[i]
-		if count, ok := m[sum-k]; ok {
+		sum += nums[i] // sum为当前的前缀和
+		if count, ok := m[sum-k]; ok { // 如果在map中找到了某个前缀和x,sum-x==k，表明找到了count个连续子数组的和为k
 			res += count
 		}
-		m[sum]++
+		m[sum]++ // 该前缀和出现的次数+1
 	}
 	return res
 }
