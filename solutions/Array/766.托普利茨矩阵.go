8122e3e76c9b4fad813e313782afc80293d05229
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-17 17:20:02 +0800

    update solution for problem766

--- solutions/Array/766.托普利茨矩阵.go
+++ solutions/Array/766.托普利茨矩阵.go
@@ -6,34 +6,48 @@
 
 // @lc code=start
 func isToeplitzMatrix(matrix [][]int) bool {
+	// 满足托普利茨矩阵定义的矩阵，从第二行开始，每个元素等于其左上方的元素(跳过第一列)
 	m, n := len(matrix), len(matrix[0])
-
-	for j := n-1; j >= 0; j-- {
-		pivot := 100
-		for k := 0; k+j < n && k < m; k++ {
-			if k == 0 {
-				pivot = matrix[k][k+j]
-			} else {
-				if matrix[k][k+j] != pivot {
-					return false
-				}
-			}
-		}
-	}
-	
 	for i := 1; i < m; i++ {
-		pivot := 100
-		for k := 0; k+i < m && k < n; k++ {
-			if k == 0 {
-				pivot = matrix[i+k][k]
-			} else {
-				if matrix[i+k][k] != pivot {
-					return false
-				}
+		for j := 1; j < n; j++ {
+			if matrix[i][j] != matrix[i-1][j-1] {
+				return false
 			}
 		}
 	}
 	return true
 }
+
+
+// func isToeplitzMatrix(matrix [][]int) bool {
+// 	m, n := len(matrix), len(matrix[0])
+
+// 	for j := n-1; j >= 0; j-- {
+// 		pivot := 100
+// 		for k := 0; k+j < n && k < m; k++ {
+// 			if k == 0 {
+// 				pivot = matrix[k][k+j]
+// 			} else {
+// 				if matrix[k][k+j] != pivot {
+// 					return false
+// 				}
+// 			}
+// 		}
+// 	}
+	
+// 	for i := 1; i < m; i++ {
+// 		pivot := 100
+// 		for k := 0; k+i < m && k < n; k++ {
+// 			if k == 0 {
+// 				pivot = matrix[i+k][k]
+// 			} else {
+// 				if matrix[i+k][k] != pivot {
+// 					return false
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return true
+// }
 // @lc code=end
 
