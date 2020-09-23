6d7d30acb07ca5423b5b3d63f299e12b5b76cd6d
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-23 13:47:41 +0800

    add solution for problem482

---
+++ solutions/Unknown/482.密钥格式化.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=482 lang=golang
+ *
+ * [482] 密钥格式化
+ */
+
+// @lc code=start
+func licenseKeyFormatting(S string, K int) string {
+	var res bytes.Buffer
+	strb := []byte(strings.Join(strings.Split(strings.ToUpper(S), "-"), ""))
+	n, part1Len, cnt := len(strb), 1, 0
+	
+	for part1Len < n {
+		if (n-part1Len) % K == 0 {
+			break
+		}
+		part1Len++
+	}
+
+	for i, ch := range strb {
+		if i >= part1Len {
+			if cnt % K == 0 {
+				res.WriteByte('-')
+			}
+			cnt++
+		}	
+		res.WriteByte(ch)
+	}
+	return res.String()
+}
+// @lc code=end
+
