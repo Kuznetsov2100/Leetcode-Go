12ffbe7fd3f6f4cef7c02991bf17c46885299b92
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-24 21:00:36 +0800

    add solution for problem1475

---
+++ solutions/Stack/1475.商品折扣后的最终价格.go
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1475 lang=golang
+ *
+ * [1475] 商品折扣后的最终价格
+ */
+
+// @lc code=start
+func finalPrices(prices []int) []int {
+	var stack []int // 单调递增栈，栈内保存的是prices数组中的元素索引
+	res := make([]int, len(prices))
+	copy(res, prices)
+	for i, price := range prices {
+		for len(stack) > 0 && prices[stack[len(stack)-1]] >= price {
+			res[stack[len(stack)-1]] = prices[stack[len(stack)-1]]-price
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+	return res
+}
+// @lc code=end
+
