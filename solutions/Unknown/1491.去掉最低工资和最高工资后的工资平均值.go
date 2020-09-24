f517e6dfd4a02a9e0ce50b0f86a9e5473b5924f2
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-24 20:18:00 +0800

    add solution for problem1491

---
+++ solutions/Unknown/1491.去掉最低工资和最高工资后的工资平均值.go
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1491 lang=golang
+ *
+ * [1491] 去掉最低工资和最高工资后的工资平均值
+ */
+
+// @lc code=start
+func average(salary []int) float64 {
+	maximum, minimum := math.MinInt64, math.MaxInt64
+	sum := 0
+	for _, val := range salary {
+		sum += val
+		if val > maximum {
+			maximum = val
+		}
+		if val < minimum {
+			minimum = val
+		}
+	}
+	return float64(sum-minimum-maximum) / float64(len(salary)-2)
+}
+// @lc code=end
+
