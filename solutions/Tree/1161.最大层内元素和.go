a42644f5235d532a85a7750d1468e9a7155f3f08
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-29 20:14:21 +0800

    update solution for problem1161

--- solutions/Tree/1161.最大层内元素和.go
+++ solutions/Tree/1161.最大层内元素和.go
@@ -14,14 +14,13 @@
  * }
  */
 func maxLevelSum(root *TreeNode) int {
-	var arr [][]int
+	var res int
 	var queue []*TreeNode
-	if root != nil {
-		queue = append(queue, root) // //将根节点放入队列中，然后不断遍历队列
-	}
-	index := 0
+	level, maxSum := 0, math.MinInt64
+
+	queue = append(queue, root) // //将根节点放入队列中，然后不断遍历队列
 	for len(queue) != 0 {
-		index++
+		level++
 		var levelsum int
 		// n := len(queue),获取当前队列的长度，这个长度相当于当前这一层的节点个数	
 		// 将队列中的元素都拿出来(也就是获取这一层的节点)，暂存到level中
@@ -37,19 +36,12 @@ func maxLevelSum(root *TreeNode) int {
 				queue = append(queue, node.Right)
 			}
 		}
-		arr = append(arr, []int{index, levelsum}) // 将这一层的元素加入结果中
-	}
-	sort.Slice(arr, func(i, j int) bool {
-		if arr[i][1] > arr[j][1] {
-			return true
-		} else if arr[i][1] == arr[j][1] {
-			if arr[i][0] < arr[j][0] {
-				return true
-			}
+		if levelsum > maxSum {
+			maxSum = levelsum
+			res = level
 		}
-		return false
-	})
-	return arr[0][0]
+	}
+	return res
 }
 // @lc code=end
 
