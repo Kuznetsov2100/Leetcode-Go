106958ea0cbf865856964326e9c680870a6ba83b
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-29 14:06:04 +0800

    update solution for problem1287

--- solutions/Unknown/1287.有序数组中出现次数超过-25-的元素.go
+++ solutions/Unknown/1287.有序数组中出现次数超过-25-的元素.go
@@ -7,22 +7,12 @@
 // @lc code=start
 func findSpecialInteger(arr []int) int {
 	var res int
-	n, count := len(arr), 1
-	threshold := n/4
-	for i := 0; i < n-1; i++ {
-		if arr[i] != arr[i+1] {
-			if count > threshold {
-				res = arr[i]
-				break
-			} else {
-				count = 1
-				continue
-			}
+	threshold := len(arr)/4
+	for i := range arr {
+		if arr[i] == arr[i+threshold] {
+			res = arr[i]
+			break
 		}
-		count++
-	}
-	if res == 0 && count > threshold {
-		res = arr[n-1]
 	}
 	return res
 }
