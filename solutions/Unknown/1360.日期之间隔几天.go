f80fd5cbc4e37b21a9ed521cc01cb9e606e50b17
handane123 <7983700+handane123@users.noreply.github.com>
2020-09-30 09:59:31 +0800

    update solution for problem1360

--- solutions/Unknown/1360.日期之间隔几天.go
+++ solutions/Unknown/1360.日期之间隔几天.go
@@ -6,19 +6,54 @@
 
 // @lc code=start
 func daysBetweenDates(date1 string, date2 string) int {
-	date1arr := strings.Split(date1, "-")
-	date2arr := strings.Split(date2, "-")
-	var arr1, arr2 []int
-	for i := 0; i < len(date1arr); i++ {
-		number1, _ := strconv.Atoi(date1arr[i])
-		number2, _ := strconv.Atoi(date2arr[i])
-		arr1 = append(arr1, number1)
-		arr2 = append(arr2, number2)
+	return abs(calDays(date1)-calDays(date2))
+}
+
+func calDays(date string) int {
+	// anchor : 1971-01-01
+	var res int
+	months := []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	arr := strings.Split(date, "-")
+	year, _ := strconv.Atoi(arr[0])
+	month, _ := strconv.Atoi(arr[1])
+	day, _ := strconv.Atoi(arr[2])
+
+	// year diff
+	for i := 1971; i < year; i++ {
+		if isLeapYear(i) {
+			res += 366
+		} else {
+			res += 365
+		}
 	}
-	
-	start := time.Date(arr1[0],time.Month(arr1[1]), arr1[2], 0, 0, 0, 0, time.UTC)
-	end := time.Date(arr2[0], time.Month(arr2[1]), arr2[2], 0, 0, 0, 0, time.UTC)
-	return abs(int(end.Sub(start)/86400000000000))
+
+	// month diff
+	for i := 1; i < month; i++ {
+		if i == 2 {
+			if isLeapYear(year) {
+				res += 29
+			} else {
+				res += 28
+			}
+			continue
+		}
+		res += months[i-1]
+	}
+	res += day-1 // day diff
+	return res
+}
+
+func isLeapYear(year int) bool {
+	if year % 100 == 0 {
+		if year % 400 == 0 {
+			return true
+		}
+	} else if year % 100 != 0 {
+		if year % 4 == 0 {
+			return true
+		}
+	}
+	return false
 }
 
 func abs(x int) int {
