c069849e0cd538e1df283730709d5b9eaa826e92
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-01 20:39:00 +0800

    add solution for problem382

---
+++ solutions/Reservoir Sampling/382.链表随机节点.go
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=382 lang=golang
+ *
+ * [382] 链表随机节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ import "math/rand"
+type Solution struct {
+        head *ListNode
+}
+
+
+/** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+func Constructor(head *ListNode) Solution {
+        return Solution{head:head}
+}
+
+
+/** Returns a random node's value. */
+func (this *Solution) GetRandom() int {
+        // 蓄水池抽样算法
+        i := 2
+        res := this.head.Val
+        cur := this.head.Next
+        for cur != nil {
+                if rand.Intn(i) == 0 {
+                    res = cur.Val
+                }
+                i++
+                cur = cur.Next
+        }
+        return res
+}
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(head);
+ * param_1 := obj.GetRandom();
+ */
+// @lc code=end
+
