92c80490556286a72025219cd432977138696854
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-13 16:54:44 +0800

    add solution for problem1137

---
+++ solutions/Array/1137.第-n-个泰波那契数.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1137 lang=golang
+ *
+ * [1137] 第 N 个泰波那契数
+ */
+
+// @lc code=start
+func tribonacci(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 || n == 2 {
+		return 1
+	}
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 1
+	dp[2] = 1
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+	}
+	return dp[n]
+}
+// @lc code=end
+
