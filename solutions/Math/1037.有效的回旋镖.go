a03eaa08e52d3670c789d48bdbec89faf654d9cb
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-13 19:37:15 +0800

    update solution for problem1037

--- solutions/Math/1037.有效的回旋镖.go
+++ solutions/Math/1037.有效的回旋镖.go
@@ -6,18 +6,15 @@
 
 // @lc code=start
 func isBoomerang(points [][]int) bool {
-	if (points[0][0] == points[1][0] && points[0][1] == points[1][1] ) ||
-	(points[0][0] == points[2][0] && points[0][1] == points[2][1]) {
-		return false		
-	}
+	// 根据斜率来判断， 将除法转换成叉乘，可避免除数为0以及点是否相同的判断
 	dx1 := points[0][0]-points[1][0]
 	dy1 := points[0][1]-points[1][1]
 	dx2 := points[1][0]-points[2][0]
 	dy2 := points[1][1]-points[2][1]
-	if dx2 * dy1 != dx1* dy2 {
-		return true
+	if dx2 * dy1 == dx1 * dy2 {
+		return false
 	}
-	return false
+	return true
 }
 // @lc code=end
 
