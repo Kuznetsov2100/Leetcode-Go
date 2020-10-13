2801f5747d1a729ee853cac90c20b609ab555111
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-13 18:43:48 +0800

    update solution for problem1089

--- solutions/Unknown/1089.复写零.go
+++ solutions/Unknown/1089.复写零.go
@@ -7,12 +7,9 @@
 // @lc code=start
 func duplicateZeros(arr []int)  {
 	n := len(arr)
-	for i := 0; i < n; i++ {
-		if arr[i] == 0 && i < n-1 {
-			if i+2 < n {
-				copy(arr[i+2:], arr[i+1:])
-			}	
-			arr[i+1] = 0
+	for i := 0; i < n-1; i++ {
+		if arr[i] == 0 {
+			copy(arr[i+1:], arr[i:])
 			i++
 		}
 	}
