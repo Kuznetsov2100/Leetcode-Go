91b57ee496e980d3e89b52f1e76d6fd68ff46c10
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-13 12:17:29 +0800

    add solution for problem1313

---
+++ solutions/Unknown/1313.解压缩编码列表.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1313 lang=golang
+ *
+ * [1313] 解压缩编码列表
+ */
+
+// @lc code=start
+func decompressRLElist(nums []int) []int {
+	var res []int
+	for i := 0; i < len(nums); i = i+2 {
+		for j := 0; j < nums[i]; j++ {
+			res = append(res, nums[i+1])
+		}
+	}
+	return res
+}
+// @lc code=end
+
