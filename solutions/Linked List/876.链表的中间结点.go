3ab9ca8b86e355ee8ecefff1c3506f3968a23536
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-16 18:31:37 +0800

    add solution for problem876

---
+++ solutions/Linked List/876.链表的中间结点.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=golang
+ *
+ * [876] 链表的中间结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return slow	
+}
+// @lc code=end
+
