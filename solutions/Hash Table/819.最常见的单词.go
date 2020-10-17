9722a455bc38c54a5ee0c55edd4cb05e2edd1d8d
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-17 19:56:52 +0800

    update solution for problem819

--- solutions/Hash Table/819.最常见的单词.go
+++ solutions/Hash Table/819.最常见的单词.go
@@ -5,14 +5,15 @@
  */
 
 // @lc code=start
+// 将标点符号全部替换成空格，然后使用strings.Fields来处理
 func mostCommonWord(paragraph string, banned []string) string {
 	var res string
 	max := 0
-	f := func(r rune) bool {return strings.ContainsRune(" !?',;.", r)}
+	paragraph = strings.NewReplacer("!", " ", "?", " ", ",", " ", "'", " ", ";", " ", ".", " ").Replace(paragraph)
 	m := make(map[string]int)
 	ban := make(map[string]bool)
 	for i := range banned { ban[banned[i]] = true }
-	for _, word := range strings.FieldsFunc(paragraph, f) {
+	for _, word := range strings.Fields(paragraph) {
 		word = strings.ToLower(word)
 		if !ban[word] {
 			m[word]++
@@ -24,5 +25,26 @@ func mostCommonWord(paragraph string, banned []string) string {
 	}
 	return res
 }
+
+// 用多个分隔符来分割paragraph
+// func mostCommonWord(paragraph string, banned []string) string {
+// 	var res string
+// 	max := 0
+// 	f := func(r rune) bool {return strings.ContainsRune(" !?',;.", r)}
+// 	m := make(map[string]int)
+// 	ban := make(map[string]bool)
+// 	for i := range banned { ban[banned[i]] = true }
+// 	for _, word := range strings.FieldsFunc(paragraph, f) {
+// 		word = strings.ToLower(word)
+// 		if !ban[word] {
+// 			m[word]++
+// 			if v := m[word]; v > max {
+// 				max = v
+// 				res = word
+// 			}
+// 		}	
+// 	}
+// 	return res
+// }
 // @lc code=end
 
