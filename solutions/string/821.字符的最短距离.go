dc3a752d5b74c62d8e44a4d749c8b90312f895b9
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-21 13:51:46 +0800

    add solution for problem821

---
+++ solutions/string/821.字符的最短距离.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=821 lang=golang
+ *
+ * [821] 字符的最短距离
+ */
+
+// @lc code=start
+func shortestToChar(S string, C byte) []int {
+	// 对于S中每个字符c, 维护两个变量：
+	// leftIndex: 字符c左边最近的字符C的索引
+	// rightIndex: 字符c右边最近的字符C的索引
+	res := make([]int, len(S))
+	leftIndex := -100000
+	rightIndex := strings.IndexByte(S, C)
+	for i := range S {
+		if S[i] == C {
+			res[i] = 0
+			leftIndex = rightIndex
+			if index := strings.IndexByte(S[i+1:], C); index == -1 {
+				rightIndex = 100000
+			} else {
+				rightIndex += index+1
+			}
+		} else {
+			res[i] = min(rightIndex-i, i-leftIndex)
+		}
+	}
+	return res
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+// @lc code=end
+
