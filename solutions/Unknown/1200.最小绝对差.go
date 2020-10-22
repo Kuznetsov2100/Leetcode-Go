b5a56e831b5c83e837436fcdc8dff940dff1336a
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-22 22:35:28 +0800

    add solution for problem1200

---
+++ solutions/Unknown/1200.最小绝对差.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1200 lang=golang
+ *
+ * [1200] 最小绝对差
+ */
+
+// @lc code=start
+func minimumAbsDifference(arr []int) [][]int {
+	var res [][]int
+	n, min := len(arr), math.MaxInt64
+	sort.Ints(arr)
+	for i := 0; i < n-1; i++ {
+		diff := arr[i+1]-arr[i]
+		if diff == min {
+			res = append(res, []int{arr[i], arr[i+1]})
+		} else if diff < min {
+			res = nil
+			res = append(res, []int{arr[i], arr[i+1]})
+			min = diff
+		}
+	}
+	return res
+}
+// @lc code=end
+
