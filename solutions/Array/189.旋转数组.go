1a272614fdeb6e5992e84d880536f3cadbf58ee2
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-24 20:07:11 +0800

    update solution for problem189

--- solutions/Array/189.旋转数组.go
+++ solutions/Array/189.旋转数组.go
@@ -7,12 +7,14 @@
 // @lc code=start
 func rotate(nums []int, k int)  {
 	n := len(nums)
-	if k == n {
-		return
-	} else if k > n {
-		k = k % n
+	k = k % n
+	if k != 0 {
+		res := make([]int, n)
+		copy(res, nums)
+		for i := 0; i < n; i++ {
+			nums[i] = res[(n-k+i) % n]
+		}
 	}
-	copy(nums,  append(nums[n-k:],nums[:n-k]...))
 }
 // @lc code=end
 
