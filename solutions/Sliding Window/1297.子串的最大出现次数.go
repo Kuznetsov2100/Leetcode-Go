a51c47e360c47b26153dee7c535db14e80055413
handane123 <7983700+handane123@users.noreply.github.com>
2020-11-03 17:44:33 +0800

    update solution for problem1297

--- solutions/Sliding Window/1297.子串的最大出现次数.go
+++ solutions/Sliding Window/1297.子串的最大出现次数.go
@@ -10,21 +10,31 @@ func maxFreq(s string, maxLetters int, minSize int, maxSize int) int {
 	var res int
 	n := len(s)
 	m := make(map[string]int)
-	window := make(map[byte]int) // 记录窗口中子串各字符出现的频率
+	window := make([]int, 26) // 记录窗口中子串各字符出现的频率
+	windowLetters := 0
 	for i := 0; i < minSize-1; i++ {
-		window[s[i]]++
+		window[s[i]-'a']++
+	}
+
+	for i := 0; i < 26; i++ {
+		if window[i] > 0 {
+			windowLetters++
+		}
 	}
 
 	for i := 0; i <= n-minSize; i++ {
-		candidate := s[i:i+minSize] // 当前候选子串
-		window[s[i+minSize-1]]++
-		if len(window) <= maxLetters { // 窗口子串的不同字符个数小于等于maxLetters
-			m[candidate]++
-			res = max(res, m[candidate])
+		left, right := i, i+minSize-1
+		window[s[right]-'a']++
+		if window[s[right]-'a'] == 1 {
+			windowLetters++
 		}
-		window[s[i]]--
-		if window[s[i]] == 0 { // 窗口最左侧字符出现次数降为0，则需从窗口中删除该字符
-			delete(window, s[i])
+		if windowLetters <= maxLetters { // 窗口子串的不同字符个数小于等于maxLetters
+			m[s[left:right+1]]++
+			res = max(res, m[s[left:right+1]])
+		}
+		window[s[left]-'a']--
+		if window[s[left]-'a'] == 0 { // 窗口最左侧字符出现次数降为0，则需从窗口中删除该字符
+			windowLetters--
 		}
 	}
 	return res
