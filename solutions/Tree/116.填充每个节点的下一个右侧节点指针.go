e0e456a57e6a019a05eec4d929a0e871031b8827
handane123 <7983700+handane123@users.noreply.github.com>
2020-11-05 18:41:30 +0800

    add solution for problem116

---
+++ solutions/Tree/116.填充每个节点的下一个右侧节点指针.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=116 lang=golang
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ */
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+func connect(root *Node) *Node {
+	if root == nil || (root.Left == nil && root.Right == nil) {
+		return root
+	}
+	root.Left.Next = root.Right
+	if root.Next != nil {
+		root.Right.Next = root.Next.Left
+	}
+	connect(root.Left)
+	connect(root.Right)
+	return root
+}
+// @lc code=end
+
