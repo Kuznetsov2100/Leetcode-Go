a59cef386e0ab6c0ffc0b82e08b498312c1095fc
handane123 <7983700+handane123@users.noreply.github.com>
2020-11-08 17:36:08 +0800

    add solution for problem1005

---
+++ solutions/Heap/1005.k-次取反后最大化的数组和.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=1005 lang=golang
+ *
+ * [1005] K 次取反后最大化的数组和
+ */
+
+// @lc code=start
+import "container/heap"
+func largestSumAfterKNegations(A []int, K int) int {
+	// 维护一个最小堆，每次pop最小元素，将该元素取反后push回去，执行K次后结束，最后遍历堆中元素求和。
+	var res int
+	n := len(A)
+	h := make(IntMinHeap, n)
+	copy(h, A)
+	heap.Init(&h)
+	for K > 0 {
+		heap.Push(&h, -heap.Pop(&h).(int))
+		K--
+	}
+
+	for i := 0; i < n; i++ {
+		res += h[i]
+	}
+	return res
+}
+
+type IntMinHeap []int
+func (h IntMinHeap) Len() int           { return len(h) }
+func (h IntMinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntMinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntMinHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *IntMinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+// @lc code=end
+
