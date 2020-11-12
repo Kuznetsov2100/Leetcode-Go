62bf5423ff81e0f487a3ab6f7b42c468bfe6be20
handane123 <7983700+handane123@users.noreply.github.com>
2020-11-12 20:55:43 +0800

    add solution for problem620

---
+++ solutions/sql/620.有趣的电影.sql
@@ -0,0 +1,14 @@
+--
+-- @lc app=leetcode.cn id=620 lang=mysql
+--
+-- [620] 有趣的电影
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select *
+from cinema
+where description <> 'boring' and (id % 2) <> 0
+order by rating desc
+-- @lc code=end
+
