85984d70bb45216f335073f4642fcf3290ef7289
handane123 <7983700+handane123@users.noreply.github.com>
2020-11-12 20:30:07 +0800

    add solution for problem182

---
+++ solutions/sql/182.查找重复的电子邮箱.sql
@@ -0,0 +1,23 @@
+--
+-- @lc app=leetcode.cn id=182 lang=mysql
+--
+-- [182] 查找重复的电子邮箱
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select Email
+from Person
+group by Email
+having count(Email) > 1
+
+
+-- SELECT Email FROM
+-- (
+--     SELECT Email, count(Email) AS frequency
+--     FROM Person
+--     Group By Email
+-- )   AS table1
+-- WHERE frequency > 1
+-- @lc code=end
+
