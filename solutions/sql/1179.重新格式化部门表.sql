43386091e84ea36d07e9bd80b7f4cf22efb5963b
handane123 <7983700+handane123@users.noreply.github.com>
2020-11-16 19:29:46 +0800

    add solution for problem1179

---
+++ solutions/sql/1179.重新格式化部门表.sql
@@ -0,0 +1,26 @@
+--
+-- @lc app=leetcode.cn id=1179 lang=mysql
+--
+-- [1179] 重新格式化部门表
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+# SUM 函数起到遍历作用
+SELECT id,
+SUM(IF(`month`='Jan',revenue,NULL)) Jan_Revenue,
+SUM(IF(`month`='Feb',revenue,NULL)) Feb_Revenue,
+SUM(IF(`month`='Mar',revenue,NULL)) Mar_Revenue,
+SUM(IF(`month`='Apr',revenue,NULL)) Apr_Revenue,
+SUM(IF(`month`='May',revenue,NULL)) May_Revenue,
+SUM(IF(`month`='Jun',revenue,NULL)) Jun_Revenue,
+SUM(IF(`month`='Jul',revenue,NULL)) Jul_Revenue,
+SUM(IF(`month`='Aug',revenue,NULL)) Aug_Revenue,
+SUM(IF(`month`='Sep',revenue,NULL)) Sep_Revenue,
+SUM(IF(`month`='Oct',revenue,NULL)) Oct_Revenue,
+SUM(IF(`month`='Nov',revenue,NULL)) Nov_Revenue,
+SUM(IF(`month`='Dec',revenue,NULL)) Dec_Revenue
+FROM Department
+GROUP BY id;
+-- @lc code=end
+
